7a69a4d85d84a22a2d8d92014d5e324db4000d61
VarunJoshi10 <72180614+VarunJoshi10@users.noreply.github.com>
2023-06-27 19:29:18 +0000

    getting CLI input

---
+++ calculator/target/debug/calculator.d
@@ -0,0 +1,1 @@
+/home/runner/Rust-Learning/calculator/target/debug/calculator: /home/runner/Rust-Learning/calculator/src/main.rs
---
+++ calculator/target/debug/deps/calculator-6592735afeaf18b7.d
@@ -0,0 +1,5 @@
+/home/runner/Rust-Learning/calculator/target/debug/deps/calculator-6592735afeaf18b7: src/main.rs
+
+/home/runner/Rust-Learning/calculator/target/debug/deps/calculator-6592735afeaf18b7.d: src/main.rs
+
+src/main.rs:
